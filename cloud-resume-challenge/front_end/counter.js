44e86b2d856772abe77dc9ebdba671e0a307ec1b
unknown <desmond.witherspoon@gmail.com>
2022-09-13 23:09:10 -0400

    updated front end

--- cloud-resume-challenge/front_end/counter.js
+++ cloud-resume-challenge/front_end/counter.js
@@ -1,8 +1,11 @@
-let url = new URL('https://32dyhayxu7.execute-api.us-east-1.amazonaws.com/prod/increment');
+
+//let url = new URL('https://32dyhayxu7.execute-api.us-east-1.amazonaws.com/prod/increment');
 
 async function fetchTest() {
-    let response = await fetch(url);
-    let responseText = await response.text();
+    let response = await fetch(
+        "https://32dyhayxu7.execute-api.us-east-1.amazonaws.com/prod/increment"
+    );
+    let responseText = await response.json();
 
     document.getElementById('value').innerHTML = responseText;
     
