c35f001a075d87e5583f4d65df43b54e73a3473e
unknown <desmond.witherspoon@gmail.com>
2022-09-15 17:22:32 -0400

    added invalidations

---
+++ cloud-resume-challenge/back_end/lambda_function.py
@@ -0,0 +1,23 @@
+import boto3, json
+
+client = boto3.client('dynamodb')
+TableName = 'cloudResumeCounter'
+
+def lambda_handler(event, context):
+
+    resp = client.update_item(
+        TableName='cloudResumeCounter',
+        Key={
+            'stat': {'S': 'viewCount'}
+        },
+        UpdateExpression='ADD quantity :q',
+        ExpressionAttributeValues={":q": {"N": "1"}},
+        ReturnValues='UPDATED_NEW'
+    )
+
+    value = resp['Attributes']['quantity']['N']
+    print(resp['Attributes'])
+    print("UpdateItem succeeded:")
+    # print(json.dumps(resp, indent=4))
+
+    return(value)
