4c7fe54a7a28c2c26a72af833e5d8f595ebfdb35
unknown <desmond.witherspoon@gmail.com>
2022-09-14 23:04:48 -0400

    initial sync

--- cloud-resume-challenge/front_end/count.js
+++ cloud-resume-challenge/front_end/count.js
@@ -1,4 +1,4 @@
-let url = new URL('https://fu3ei0clah.execute-api.us-east-1.amazonaws.com/prod');
+let url = new URL('https://32dyhayxu7.execute-api.us-east-1.amazonaws.com/prod/increment');
 
 async function fetchTest() {
     let response = await fetch(url);
--- cloud-resume-challenge/front_end/counter.js
+++ cloud-resume-challenge/front_end/counter.js
@@ -1,16 +1,27 @@
 
 //let url = new URL('https://32dyhayxu7.execute-api.us-east-1.amazonaws.com/prod/increment');
 
-async function fetchTest() {
-    let response = await fetch(
-        "https://32dyhayxu7.execute-api.us-east-1.amazonaws.com/prod/increment"
-    );
-    let responseText = await response.json();
+//async function fetchTest() {
+  //  let response = await fetch(
+    //    "https://32dyhayxu7.execute-api.us-east-1.amazonaws.com/prod/increment"
+    //);
+    //let responseText = await response.json();
 
-    document.getElementById('value').innerHTML = responseText;
+    //document.getElementById('value').innerHTML = responseText;
     
+//}
+
+//(async() => {
+//    await fetchTest();
+//})();
+
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+  let response = await fetch(
+    "https://32dyhayxu7.execute-api.us-east-1.amazonaws.com/prod/increment"
+  );
+  let data = await response.json();
+  counter.innerHTML = `You are the ${data}. visitor to my Cloud Resume Challenge site`;
 }
 
-(async() => {
-    await fetchTest();
-})();
+updateCounter();
